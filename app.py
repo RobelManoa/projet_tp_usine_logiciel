c1215052dd82eb004354e78dfeae5fc40cf0e8b8
RobelManoa <manoarobel@gmail.com>
2026-04-10 16:08:14 +0400

    feat: ajouter la fonction calculer TVA

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# fonction calculer_remise(prix, taux)
-def calculer_remise(prix, taux):
-    remise = prix * (taux / 100)
-    return remise
+# fonction calcule TVA
+def calculer_tva(prix, taux=0.20):
+    tva = prix * (taux / 100)
+    return tva
